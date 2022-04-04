fd1e450c011eb8a6594030c41c1a108a08a99c8a
Abinash <avinashniure026@gmail.com>
2022-04-04 12:19:24 +0930

    session data and user passing

--- mainfile/auth.py
+++ mainfile/auth.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template,request,flash,redirect,url_for
+from flask import Blueprint, render_template,request,flash,redirect,url_for,session
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+
+
 auth = Blueprint('auth', __name__)
 
 @auth.route('/signup', methods=['GET','POST'])
@@ -13,16 +15,24 @@ def signup():
         password = request.form.get('password')
         password1 = request.form.get('password1')
 
-        if len(password)<8:
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('User Already exist',category='error')
+        elif len(password)<8:
             flash('Not sufficent length for password',category='error')
-        if password != password1:
+        elif password != password1:
             flash('two passwords do not match',category='error')
         else:
             new_user = User(email=email, first_name= first_name, last_name=last_name, password=generate_password_hash(password, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             flash('account created', category='sucess')
-            return redirect(url_for('views.home'))
+            session['user_id'] = user.id
+            return redirect(url_for('views.index))
+    else:
+        if "user_id" in session:
+            return redirect(url for("views.index"))
     return render_template("signup.html")
     
     
@@ -35,13 +45,20 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
-                return redirect(url_for('views.home'))
+                session['user_id'] = user.id
+                return redirect(url_for('views.index'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
             flash('Email does not exist.', category='error')
+    else:
+        if "user_id" in session:
+            return redirect(url for("views.index"))
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return "logout"
+   session.pop("user_id", None)
+
+
+    return redirect(url_for('views.home'))
--- mainfile/views.py
+++ mainfile/views.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect, url_for
+from . import db
+from .models import User
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html",user=None)
+
+
+@views.route('/index')
+def index():
+    if "user_id" in session:
+        id = session["user_id"]
+        user = User.query.filter_by(id=id).first()
+        return render_template("index.html", user=user)
+    else:
+        return redirect (url_for("auth.login"))
